da4e0b33033d646523f2bd0c6d8568bae96a0003
unknown <danielsafari143@gmail.com>
2023-12-04 20:20:04 +0100

    ADD CONSTRUCTOR

--- dog.java
+++ dog.java
@@ -1,12 +1,17 @@
 package oop;
 
 class Dog {
+    public void dog(){
+        int color = 23422;
+        System.err.println(color);
+    }
+
     String breed;
     int age;
     String color;
 
-    void barking(){}
-    void hungry(){}
-    void sleeping(){}
+    void barking(){} // Methods/Behavior
+    void hungry(){} // Methods/Behavior
+    void sleeping(){} // Methods/Behavior
 }
 
