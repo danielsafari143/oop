33be84cb748df0b8791643b2ff3f2f8ac1d9603e
unknown <danielsafari143@gmail.com>
2023-12-05 23:07:04 +0100

    ADD TESTS

--- Animal.java
+++ Animal.java
@@ -2,10 +2,11 @@ package oop;
 
 public class Animal {
     String name;
-    
+
     public Animal(int color , int age ){
         System.out.println("Hi Animals");
     }
+    
     protected String cries(String spec){
         return "This animal is " + spec;
     }
--- Main.java
+++ Main.java
@@ -34,7 +34,7 @@ public class Main {
 
     public static void main(String[] args) {
         Dog rachel = new Dog("Rchel");
-        boolean isDogAnAnimal = rachel instanceof Animal;
+        boolean isDogAnAnimal = rachel instanceof Dog;
         System.out.println(rachel.cries("Speaking"));
         System.out.println(isDogAnAnimal);
     };
