1403a5b2792380ef1702ad944ccf543168d79b35
unknown <danielsafari143@gmail.com>
2023-12-07 19:17:07 +0100

    ADD CHANGES

--- CountMinutes.java
+++ CountMinutes.java
@@ -1,7 +1,9 @@
 package oop;
+import java.time.LocalTime;
 
 public class CountMinutes extends Thread {
     int counterName;
+    LocalTime time;
 
     CountMinutes(int counterName){
         this.counterName = counterName;
@@ -11,8 +13,10 @@ public class CountMinutes extends Thread {
     public void run(){
        try {
          for(int i = 0; i < 5; i++){
+            this.time = LocalTime.now();
             System.out.println("The result of counter"+ counterName +" is there :" + i);
             Thread.sleep(3000);
+            System.out.println(time);
          }
        } catch (Exception e) {
          System.out.println(e);
--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 package oop;
 
+import java.lang.reflect.Array;
+
 public class Main {
     static int a = 1;
     static int b = 11;
@@ -33,11 +35,12 @@ public class Main {
     }
 
     public static void main(String[] args) {
+        int places [] = {1,2,3,4};
        CountMinutes counter = new CountMinutes(1);
        CountMinutes counter2 = new CountMinutes(2);
        CounterSecond counterSec = new CounterSecond();
        Thread exec = new Thread(counterSec);
-       
+        System.out.println(places[1]);
        exec.start();
        counter2.start();
        counter.start();
