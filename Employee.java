e5bb08b368097d97599b903bc7e743aed1a908cb
unknown <danielsafari143@gmail.com>
2023-12-07 19:55:25 +0100

    ADD CHANGES

--- Employee.java
+++
@@ -1,9 +0,0 @@
-package oop;
-
-abstract class Employee{
-    private String name;
-    private String address;
-    private int number;
-
-    public abstract double computePay();
-}
--- Main.java
+++ Main.java
@@ -2,39 +2,12 @@ package oop;
 import java.util.HashSet;
 import java.util.Iterator;
 
-import java.lang.reflect.Array;
-
 public class Main {
     static int a = 1;
     static int b = 11;
     static double c = 7.3;
     static double d = 9.4;
     static String name = "Safari";
-
-    static double result1 = minFunction(c , d);
-
-    public static int minFunction(int a , int b){
-        int min;
-        
-        if(a < b) {
-            min = a;
-        }
-        else
-            min = b;
-
-        return min;
-    }
-    public static double minFunction(double a , double b){
-        double min;
-        
-        if(a < b) {
-            min = a;
-        }
-        else
-            min = b;
-
-        return min;
-    }
 
     public static void main(String[] args) {
        HashSet<String> cars = new HashSet<String>();
